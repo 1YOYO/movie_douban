9461a89932bd7057b73c0278e6ba448ff50ab0fc
unknown <>
2016-10-03 15:25:58 +0800

    第三个版本 jsonpz设置完成并成功调取豆瓣API上的数据

--- app/servie.js
+++ app/servie.js
@@ -1,28 +1,28 @@
 (function(angular) {
     'use strict';
     var app = angular.module("app");
-    app.service('httpserver',[function(){
-    	this.jsonp = function(url, data, fn){
-    		function getData(data){
-    			fn(data);
-    			document.body.removeChild(script);
-    		}
-    		window['getData'] = getData ;
-    		var serchData = '?';
-    		for(var i in data){
-    			searchData += i + "=" + data[i] + "&";
-    		}
-    		var script = document.createElement("script");
-		
-			script.src = url + searchData + "callback=getData";
-			console.log(script.src);
-		
-			document.body.appendChild(script);
+    /*建立jsonp函数*/
+	app.service('httpService', [function(){
+        this.jsonp = function(url, data, fn){
+            /*成功调取数据后要执行的回掉函数*/
+            function getData(data){
+                fn(data);
+                document.body.removeChild(script);
+            }
+            window['getData'] = getData ;
+            /*searchData的格式*/
+            var searchData = '?';
+            for(var i in data){
+                searchData = searchData + i + "=" + data[i] + "&";
+            }
 
-    	}
-    }]);
-  
-			
+           /* 1.创建一个script标签并添加属性插入到body体内*/
+           var script = document.createElement("script");
+           script.src = url + searchData + "callback=getData";
+           console.log(script.src);
+          document.body.appendChild(script);
+        }
+    }])		
 			
 			
 			
--- view/future/controller.js
+++ view/future/controller.js
@@ -1,6 +1,19 @@
 ;(function(angular){
     var app = angular.module('app');
-    app.controller('futurecontroller',['$scope',function($scope){
+   app.controller('futurecontroller',['$scope','httpService', function($scope, httpService){
+		$scope.datalist ={}
        
-    }])
+        function getMovie(start) {
+            var url = "http://api.douban.com/v2/movie/coming_soon";
+            httpService.jsonp(url, {
+                start:start, 
+                count: 10
+            }, function(data) {
+                console.log(data);
+                $scope.datalist = data;
+                $scope.$apply();
+            }); 
+        }
+        getMovie(0);
+	}]);
 })(angular)
