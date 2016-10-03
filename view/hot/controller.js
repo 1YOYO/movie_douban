7e3a9d0b9be67d2418834e9eb2a1a53158190de9
unknown <>
2016-10-03 17:21:36 +0800

    第五个版本 分页制作完成

--- view/hot/controller.js
+++ view/hot/controller.js
@@ -1,7 +1,14 @@
 (function(angular){
 	var app = angular.module("app");
 	app.controller('hotcontroller',['$scope','httpService', function($scope, httpService){
-		$scope.datalist ={}
+		$scope.datalist ={} ;
+        
+        var pageSize = 10;
+        var page = 1;
+        var start = pageSize * (page-1);
+
+        $scope.totalPage = 1;
+        $scope.currentPage = 1 ;
        
         function getMovie(start) {
             var url = "http://api.douban.com/v2/movie/in_theaters";
@@ -10,9 +17,26 @@
                 count: 10
             }, function(data) {
                 $scope.datalist = data;
+                $scope.totalPage = Math.ceil(data.total/pageSize);
                 $scope.$apply();
             }); 
         }
         getMovie(0);
+
+        $scope.goNext = function(currentPage) {
+            if(currentPage <= $scope.totalPage) {
+                var start = pageSize * (currentPage - 1);
+                getMovie(start);
+                $scope.currentPage++;
+            }
+        };
+
+        $scope.goPre = function(currentPage) {
+            if(currentPage >= 1) {
+                var start = pageSize * (currentPage - 1);
+                getMovie(start);
+                $scope.currentPage--;
+            }
+        };
 	}]);
 })(angular)
